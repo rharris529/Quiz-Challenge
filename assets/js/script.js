614caac56168c888c942800ca980fbe3748f57de
rharris529 <jordandine@gmail.com>
2022-01-20 23:41:30 -0600

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var questionOne = ["Triforce of Power", "Triforce of Wisdom", "Triforce of Courage", "Triforce of Strength"];
+var questionTwo = ["Epona", "Agro", "Seabiscuit", "Shadowmere"];
+var questionThree = ["A + L + R + Start", "A, B, A, C, A, B, B", "Down, Forward + Punch", "Up, Up, Down, Down, Left, Right, Left, Right, B, A"];
+var questionFour = ["Bullymongs", "Stairs", "Magnets", "It's own hubris"];
+var questionFive = ["Reese's Pieces", "Kool Aid", "Friendship", "Phones"];
